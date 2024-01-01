8a9b1b81c2a633003f7c109434a029037d8945e9
Addy Famuyiwa <adedoyinfamuyiwa@gmail.com>
2024-01-01 18:54:01 -0500

    Basic about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,3 +1,28 @@
+import profileImage from "../images/1700146765465.jpg"
+
 export default function About() {
-    return <h1>About</h1>
+    return (
+        <div className="App">
+          <header className="App-header">
+            <div className="content">
+                <div className= "text">
+                    <p>
+                    Hello! My name is Adedoyin Famuyiwa. I'm a student at Purdue University,
+                    majoring in Mathematics and Computer Science. My fascination with the 
+                    beauty of mathematics has always driven my passion for problem-solving.
+                        I bring those solutions to life through computer science, transforming
+                        ideas into tangible creations.
+                    </p>
+                    <p stle={{ fontSize: "14px"}}>
+                    Find my current projects and experiences below.
+                    </p>
+                </div>
+                <div className="profile-image">
+                    <img src={profileImage} alt="Profile" />
+                </div>
+                </div>
+            
+          </header>
+        </div>
+      );
 }
