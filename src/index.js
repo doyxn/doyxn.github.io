9f8061d961f434500a654df001d97d19dc74c0a4
Addy Famuyiwa <adedoyinfamuyiwa@gmail.com>
2024-02-15 05:52:18 -0500

    reworking

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './stylesheets/index.css';
 import './stylesheets/App.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter as Router } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
 
-ReactDOM.render(
-    <Router>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
       <App />
-    </Router>,
-    document.getElementById('root')
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
