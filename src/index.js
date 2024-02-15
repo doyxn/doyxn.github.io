5c790f2a2fdb69898f1d8e6b0580773abe85312e
Addy Famuyiwa <adedoyinfamuyiwa@gmail.com>
2024-02-15 05:48:32 -0500

    url fix

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './stylesheets/index.css';
 import './stylesheets/App.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter as Router } from "react-router-dom"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
+ReactDOM.render(
+    <Router>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    </Router>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
