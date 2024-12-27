247f0e0924fba2743a25b98322dd59c6af9a3fb6
Adedoyin Famuyiwa <adedoyinfamuyiwa@gmail.com>
2024-12-27 00:07:26 -0500

    Navbar Header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,45 @@
 import React from 'react';
-import '../stylesheets//Navbar.css'; // Add custom styling
+import '../stylesheets/App.css';
+import headshot from "./images/headshot.png"
 
-function Navbar() {
+const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="navbar-logo">
-        Adedoyin Famuyiwa
-      </div>
-      <ul className="navbar-links">
-        <li><a href="#about">About</a></li>
-        <li><a href="#projects">Projects</a></li>
-        <li><a href="#experience">Experience</a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
-    </nav>
+    <>
+      <header className="header-container">
+        <div className="profile-card">
+          <img
+            src={headshot} // Replace with actual image URL
+            alt="Profile"
+            className="profile-picture"
+          />
+          <div className="profile-info">
+            <h1>Aidan Andrews</h1>
+            <p>Mathematics & Computer Science, Aspiring Savant</p>
+            <p>West Lafayette, Indiana</p>
+            {/* <div className="contact-links">
+              <a href="#email">Email</a> | <a href="#phone">Phone</a> |{' '}
+              <a href="#linkedin">LinkedIn</a> | <a href="#github">GitHub</a> |{' '}
+              <a href="#instagram">Instagram</a> | <a href="#youtube">YouTube</a>
+            </div> */}
+          </div>
+        </div>
+      </header>
+      <nav className="navigation-bar">
+        <a href="#about" className="nav-link">
+          About
+        </a>
+        <a href="#projects" className="nav-link">
+          Projects
+        </a>
+        <a href="#bookshelf" className="nav-link">
+          Bookshelf
+        </a>
+        <a href="#blog" className="nav-link">
+          Blog
+        </a>
+      </nav>
+    </>
   );
-}
+};
 
 export default Navbar;
