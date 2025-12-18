630cd16c959db23123d71e1bcb31752eee9cc4dc
Adedoyin Famuyiwa <adedoyinfamuyiwa@gmail.com>
2025-12-17 21:11:58 -0500

    github workflow to sync blogs from stack repo

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import About from './pages/About/About';
 import Projects from './pages/Projects/Projects';
-import Blog from './pages/Blog/Blog';
-import BlogPost from "./pages/Blog/BlogPost"; 
+// import Blog from './pages/Blog/Blog';
 import './stylesheets/card.css';
 import './stylesheets/App.css';
 import './stylesheets/index.css';
@@ -18,8 +17,7 @@ function App() {
           <Route path='/' element = {<About />} />
           <Route path="/about" element = {<About />} />
           <Route path="/projects" element = {<Projects />} />
-          <Route path="/blog" element = {<Blog />} />
-          <Route path="/blog/:fileName" element={<BlogPost />} />
+          {/* <Route path="/blog" element = {<Blog />} /> */}
         </Routes>
         <Footer />
       </div>
